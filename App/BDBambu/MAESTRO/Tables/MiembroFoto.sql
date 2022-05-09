129ac03e42126ba3b65c588b489aef897433ef72
Richard Zapata <richard.zapata.c@gmail.com>
2022-05-09 17:26:43 -0500

    actualizacion BD

---
+++ App/BDBambu/MAESTRO/Tables/MiembroFoto.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [MAESTRO].[MiembroFoto] (
+    [MiembroFotoId] INT           IDENTITY (1, 1) NOT NULL,
+    [MiembroId]     INT           NOT NULL,
+    [Foto]          VARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([MiembroFotoId] ASC),
+    FOREIGN KEY ([MiembroId]) REFERENCES [MAESTRO].[Miembro] ([MiembroId])
+);
+
